cc0ade51d128e8fafa2d4004f3bff707204627d6
Francoise <francoise.ruch@edu.devinci.fr>
2022-02-08 18:57:51 +0100

    scrapeDedicatedAll

--- server/sandbox.js
+++ server/sandbox.js
@@ -2,13 +2,13 @@
 const dedicatedbrand = require('./sites/dedicatedbrand');
 const adresse = require('./sites/adresse');
 
-
+'https://adresse.paris/630-toute-la-collection'
 'https://www.dedicatedbrand.com/en/loadfilter?'
-async function sandbox (eshop ='https://adresse.paris/630-toute-la-collection'){
+async function sandbox (eshop ='https://www.dedicatedbrand.com/en/loadfilter?'){
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await adresse.scrape(eshop);
+    const products = await dedicatedbrand.scrape(eshop);
     console.log(products.length)
     console.log(products);
     console.log('done');
--- server/sites/dedicatedbrand.js
+++ server/sites/dedicatedbrand.js
@@ -10,12 +10,12 @@ const {'v5': uuidv5} = require('uuid');
 const parse = data => {
   const $ = cheerio.load(data);
 
-  return $('.productList-container .productList')
+  return $('data-new-gr-c-s-check-loaded')
     .map((i, element) => {
-      const link = `https://www.dedicatedbrand.com${$(element)
-        .find('.productList-link')
-        .attr('href')}`;
-
+      const link = `https://www.dedicatedbrand.com/en/loadfilter?category=men?${$(element)
+        .find('body')
+        .attr('word-wrap: break-word; white-space: pre-wrap;')}`;
+        
       return {
         link,
         'brand': 'dedicated',
@@ -30,8 +30,7 @@ const parse = data => {
           .trim()
           .replace(/\s/g, ' '),
         'photo': $(element)
-          .find('.productList-image img')
-          .attr('data-src'),
+          .find('.productList-image img').attr('data-src'),
         '_id': uuidv5(link, uuidv5.URL)
       };
     })
@@ -48,9 +47,20 @@ module.exports.scrape = async url => {
     const response = await fetch(url);
 
     if (response.ok) {
-      const body = await response.text();
-
-      return parse(body);
+      const body = await response.json();
+      let final = [];
+        body.products.forEach((element) => {
+            if(element.name!=undefined)
+      {final.push({
+        name: element["name"],
+        brand: "dedicated",
+        price: parseFloat(element["price"].price),
+        image: element["image"][0],
+        link: "https://www.dedicatedbrand.com/en/" + element["canonicalUri"],
+        _id: uuidv5("https://www.dedicatedbrand.com/en/" + element["canonicalUri"], uuidv5.URL)
+      })};
+    });
+      return final;
     }
 
     console.error(response);
