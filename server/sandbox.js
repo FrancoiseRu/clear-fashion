e2c992921a09878eb4088e750f917536422f52e3
Francoise <francoise.ruch@edu.devinci.fr>
2022-02-08 18:44:54 +0100

    scrapeAdresse

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sites/dedicatedbrand');
+const adresse = require('./sites/adresse');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+
+'https://www.dedicatedbrand.com/en/loadfilter?'
+async function sandbox (eshop ='https://adresse.paris/630-toute-la-collection'){
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
-
+    const products = await adresse.scrape(eshop);
+    console.log(products.length)
     console.log(products);
     console.log('done');
     process.exit(0);
---
+++ server/sites/adresse.js
@@ -0,0 +1,62 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const {'v5': uuidv5} = require('uuid');
+
+/**
+ * Parse webpage restaurant
+ * @param  {String} data - html response
+ * @return {Object} restaurant
+ */
+const parse = data => {
+  const $ = cheerio.load(data);
+
+  return $('.left-block')
+    .map((i, element) => {
+      const link =$(element)
+      .find('.product-image-container a')
+      .attr('href');
+
+      return {
+        link,
+        'brand': 'adresse paris',
+        'price': 
+          parseFloat($(element).parent()
+            .find('.price')
+            .text())
+        ,
+        'name': $(element).parent()
+            .find('.product-name-container a')
+            .attr('title'),
+        'photo': $(element)
+          .find('.product-image-container a img')
+          .attr('data-original'),
+
+        '_id': uuidv5(link, uuidv5.URL)
+      };
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+ module.exports.scrape = async url => {
+    try {
+      const response = await fetch(url);
+  
+      if (response.ok) {
+        const body = await response.text();
+        return parse(body);
+      }
+  
+      console.error(response);
+  
+      return null;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
+  
